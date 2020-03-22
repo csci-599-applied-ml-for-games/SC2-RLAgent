2196d2299d2a2ba4da9b1b44934afb5f6b6afbbb
ZhengyiWang <wbswzy@163.com>
2020-03-21 18:40:10 -0700

    A new version
    1. 会升级折跃，闪烁并且追猎者会使用技能，通过水晶塔折跃追猎者
    2. 折跃建筑后会探机会自动归位，会自动补充采气的农民（正常速度下可以尝试比如移走几个农民，电脑会自动归为）
    3.感觉追猎者还是不负鸡猎者的名字，还不如升级使徒有效果 打算改成使徒一波的战术

---
+++ Test/simple.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 09:51:18 2020
+
+@author: 54365
+"""
+
+import sc2
+from sc2 import run_game, maps, Race, Difficulty
+from sc2.player import Bot, Computer
+from sc2.constants import NEXUS,PYLON,ASSIMILATOR,GATEWAY,FORGE,CYBERNETICSCORE,ROBOTICSFACILITY,TWILIGHTCOUNCIL,PROBE,ZEALOT,STALKER,IMMORTAL
+import random
+
+
+class SentdeBot(sc2.BotAI):
+    def __init__(self):
+        self.ITERATIONS_PER_MINUTE = 260
+        self.MAX_WORKERS = 40  # 限制最大农民数
+
+    async def on_step(self, iteration: int):  # iteration类似游戏时钟 每分钟165个迭代(待确认)
+        self.iteration = iteration
+        await self.distribute_workers()
+        await self.build_workers()
+        await self.build_pylons()
+        await self.build_assimilators()
+        await self.expand()
+        await self.offensive_force_buildings()
+        await self.build_offensive_force()
+        await self.attack()
+
+    async def build_workers(self):
+        """
+        选择空闲基地建造农民
+        noqueue意味着当前建造列表为空
+        """
+        if len(self.units(NEXUS)) * 24 > len(self.units(PROBE)):  # 每矿农民补满就不补了
+            if len(self.units(PROBE)) < self.MAX_WORKERS:
+                for nexus in self.units(NEXUS).ready.noqueue:
+                    if self.can_afford(PROBE):
+                        await self.do(nexus.train(PROBE))
+
+    async def build_pylons(self):
+        """
+        人口空余不足5时造水晶。
+        """
+        if self.supply_left < 5 and not self.already_pending(PYLON):
+            nexuses = self.units(NEXUS).ready
+            if nexuses.exists:
+                if self.can_afford(PYLON):
+                    await  self.build(PYLON, near=nexuses.first)  # near表示建造地点。后期可以用深度学习优化
+
+    async def build_assimilators(self):
+        """
+        建造气矿
+        """
+        for nexus in self.units(NEXUS).ready:
+            vespenes = self.state.vespene_geyser.closer_than(25.0, nexus)
+            for vespene in vespenes:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vespene.position)
+                if worker is None:
+                    break
+                if not self.units(ASSIMILATOR).closer_than(1.0, vespene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vespene))
+
+    async def expand(self):
+        """
+        何时扩张 简化版
+        基地数量少于2个就立即扩张
+        """
+        if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+            await self.expand_now()
+
+    async def offensive_force_buildings(self):
+        """
+        建造产兵/科技建筑
+        """
+        # print('iterations:', self.iteration / self.ITERATIONS_PER_MINUTE)
+        # print('iterations:', self.iteration)
+        if self.units(PYLON).ready.exists:
+            pylon = self.units(PYLON).ready.random
+            # 建造BY
+            if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
+                if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
+                    await self.build(CYBERNETICSCORE, near=pylon)
+            # 建造更多BG
+            elif len(self.units(GATEWAY)) < ((self.iteration / self.ITERATIONS_PER_MINUTE) / 2):  # 粗略计算
+                if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY) and self.units(GATEWAY).amount < 8:
+                    await self.build(GATEWAY, near=pylon)
+            # 这个VS放的早啊
+            if self.units(CYBERNETICSCORE).ready.exists:
+                if len(self.units(ROBOTICSFACILITY)) < ((self.iteration / self.ITERATIONS_PER_MINUTE) / 2):
+                    if self.can_afford(ROBOTICSFACILITY) and not self.already_pending(ROBOTICSFACILITY) and self.units(ROBOTICSFACILITY).amount < 2:
+                        await self.build(ROBOTICSFACILITY, near=pylon)
+
+    async def build_offensive_force(self):
+        """
+        建造战斗单位
+        """
+        for gw in self.units(GATEWAY).ready.noqueue:
+            if not self.units(STALKER).amount > self.units(IMMORTAL).amount:  # 粗略判断
+                if self.can_afford(STALKER) and self.supply_left > 0:
+                    await self.do(gw.train(STALKER))
+
+        for sg in self.units().ready.noqueue:
+            if self.can_afford(IMMORTAL) and self.supply_left > 0:
+                await self.do(sg.train(IMMORTAL))
+
+    def find_target(self, state):
+        """
+        寻找敌方单位
+        注意这个函数不是异步的，不用加async
+        """
+        if len(self.known_enemy_units) > 0:
+            return random.choice(self.known_enemy_units)
+        elif len(self.known_enemy_structures) > 0:
+            return random.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
+
+    async def attack(self):
+        """
+        控制单位攻击视野内敌方单位
+        """
+        # {UNIT: [n to fight, n to defend] } 也是粗略计算
+        aggressive_units = {STALKER: [15, 5],
+                            IMMORTAL: [8, 3]}
+
+        for UNIT in aggressive_units:
+            # 如果数量比预定攻击单位数量多，那就主动出击，否则防守（此处疑似代码冗余）
+            if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
+                for s in self.units(UNIT).idle:
+                    await self.do(s.attack(self.find_target(self.state)))
+
+            elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
+                if len(self.known_enemy_units) > 0:
+                    for s in self.units(UNIT).idle:
+                        await self.do(s.attack(random.choice(self.known_enemy_units)))
+
+
+def main():
+    run_game(maps.get("Simple64"), [
+        Bot(Race.Protoss, SentdeBot()),
+        Computer(Race.Protoss, Difficulty.Hard)], realtime=False)  # realtime设为False可以加速
+
+
+if __name__ == '__main__':
+    main()
